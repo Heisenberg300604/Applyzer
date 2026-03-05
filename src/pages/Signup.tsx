8d26932f98867e9f397987f966c634c96f24f1f9
Anshit Sharma <anshit12005@gmail.com>
2026-03-05 20:51:14 +0530

    Add forceRedirectUrl prop to SignUp component

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/react'
 export default function SignUpPage() {
   return (
     <div className="min-h-screen gradient-hero flex items-center justify-center p-6">
-      <SignUp />
+      <SignUp forceRedirectUrl="/dashboard" />
     </div>
   )
 }
